62d19172e0d326cfa8bb349977ef04edd58055be
magica <88008125+magiaMagica@users.noreply.github.com>
2022-10-13 18:32:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import tweepy
+import time
 
-api =  tweepy.OAuthHandler( 'y4vQvfWXANPRCgkiL39paTGD8', 'lHwjDIB6c1sVqg8O0CKtQXymvCvG0cjtDFVE4hQVKBeAPNfoeJ')
-api.set_access_token('1580455034172719104-VDq0k1ATiEigAxEYZLe1K2laQ6ZpfL', 'jfHOMbGN2x8jbdO68lqrN3QWIn2eVxc7F8U5dBgz896OL')
+# Initialization
+auth = tweepy.OAuth1UserHandler(
+   "PYmsYWW9uUptZUNNiG1kYgK9d", "73AQpBn1WmHFXtHyvtqeOTSOts7X68LfgYqcRSBcFx6pLgEvz4",
+   "1580455034172719104-alsgtl5IQDKrvHwjOOthZasxEMOebw", "kb6mbOvdxjGwXuLKno3doXM7CgPtqD0cjiygfscn4BjQB"
+)
+api = tweepy.API(auth)
+
+# Getting Bot ID
+bot_id = int(api.me().id_str)
+
+mention_id = 1
