9165adcdfc3d4a5349c3fb60ac403806841fcdba
magica <88008125+magiaMagica@users.noreply.github.com>
2022-10-13 17:29:11 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+import tweepy
+
+api =  tweepy.OAuthHandler( 'y4vQvfWXANPRCgkiL39paTGD8', 'lHwjDIB6c1sVqg8O0CKtQXymvCvG0cjtDFVE4hQVKBeAPNfoeJ')
+api.set_access_token('1580455034172719104-VDq0k1ATiEigAxEYZLe1K2laQ6ZpfL', 'jfHOMbGN2x8jbdO68lqrN3QWIn2eVxc7F8U5dBgz896OL')
